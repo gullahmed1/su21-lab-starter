d537eb28ca9c1c72788c74acb91f893f2e804ca0
gullahmed1 <gull.ahmed@10xengineers.ai>
2023-04-10 18:11:05 +0500

    Updated bit_ops.c

--- lab02/bit_ops.c
+++ lab02/bit_ops.c
@@ -3,23 +3,22 @@
 
 /* Returns the Nth bit of X. Assumes 0 <= N <= 31. */
 unsigned get_bit(unsigned x, unsigned n) {
-    /* YOUR CODE HERE */
-    return (x>>n)&1; /* UPDATE WITH THE CORRECT RETURN VALUE*/
+    /* Shift x by n positions to the right and perform a bitwise AND with 1 to get the value of the nth bit */
+    return (x >> n) & 1; 
 }
 
 /* Set the nth bit of the value of x to v. Assumes 0 <= N <= 31, and V is 0 or 1 */
 void set_bit(unsigned *x, unsigned n, unsigned v) {
-    /* YOUR CODE HERE */
-    int k = 1 << n;
-    *x &= ~k;
-    *x |= (v << n) & k;
+    /* Create a mask by left-shifting 1 by n positions, then use the bitwise AND operator with the bitwise NOT of the mask to set the nth bit of x to 0. Then use the bitwise OR operator with (v << n) & mask to set the nth bit of x to v. */
+    int mask = 1 << n;
+    *x &= ~mask;
+    *x |= (v << n) & mask;
 }
 
 /* Flips the Nth bit in X. Assumes 0 <= N <= 31.*/
 void flip_bit(unsigned *x, unsigned n) {
-    /* YOUR CODE HERE */
-     int k = 1 << n;
-    *x ^= k;
-   
+    /* Create a mask by left-shifting 1 by n positions, then use the bitwise XOR operator to flip the nth bit of x. */
+     int mask = 1 << n;
+    *x ^= mask;
 }
 
