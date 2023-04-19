2fd7696f53ccf69dafe0f31564b3bbca52b02e09
gullahmed1 <gull.ahmed@10xengineers.ai>
2023-04-19 22:19:01 +0500

    Exercise 4 of lab3 completed

--- lab03/factorial.s
+++ lab03/factorial.s
@@ -22,3 +22,17 @@ main:
 
 factorial:
     # YOUR CODE HERE
+    add t0,x0,a0 #t0=n
+    addi t1,x0,1 #k=1
+    addi t2,x0,0 #i=0
+for:
+    ble t2,t0,if #i<=n
+    j done
+if:
+    ble t0,x0,done
+    mul t1,t1,t0
+    addi t0,t0,-1
+    j for
+done:
+    add a0,t1,x0
+    jr ra 
